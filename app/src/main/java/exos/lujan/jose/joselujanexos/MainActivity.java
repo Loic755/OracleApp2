1a70ba0d61e31a55fc2b8dcd14e2a84854bcd6a2
Loic755 <loic.roubaud@gmail.com>
2017-05-14 18:39:26 -0500

    App Oracle

--- app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
+++ app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
@@ -2,12 +2,23 @@ package exos.lujan.jose.joselujanexos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] elementos = {"Jose","Pedro","Mario","Miguel","Luis","Daniel","Elena","Sofia"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listview);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,elementos);
+
+        listView.setAdapter(adapter);
     }
 }
