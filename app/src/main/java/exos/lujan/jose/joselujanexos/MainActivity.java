999874c5dcb7d825894da747181c353bec3e9459
Loic755 <loic.roubaud@gmail.com>
2017-05-14 17:36:55 -0500

    Exo 25 completed

--- app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
+++ app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
@@ -2,12 +2,49 @@ package exos.lujan.jose.joselujanexos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button boton;
+    AutoCompleteTextView auto;
+    TextView texto;
+
+    String[] nombres = {"Juanito","Juan","Maria Fernanda","Laura","Miguel"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton = (Button) findViewById(R.id.boton);
+        boton.setOnClickListener(this);
+
+        auto = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        texto = (TextView) findViewById(R.id.texto);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,nombres);
+        auto.setThreshold(3);
+        auto.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+       switch(v.getId()){
+           case R.id.boton:
+               String cambio = auto.getText().toString();
+               texto.setText(cambio);
+               break;
+
+
+
+       }
+
+
+
     }
 }
