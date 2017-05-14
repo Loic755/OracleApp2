a908be86cf36da2c6fb9ce590ce8757b67dffc9e
Loic755 <loic.roubaud@gmail.com>
2017-05-14 15:14:31 -0500

    This is first test commit

--- app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
+++ app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
@@ -1,13 +1,38 @@
 package exos.lujan.jose.joselujanexos;
 
+import android.app.Dialog;
+//import android.support.v4.media.MediaDescriptionCompatApi21;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton = (Button) findViewById(R.id.button);
+        boton.setOnClickListener(this);
+    }
+
+    protected Dialog onCreateDialog(int id){
+        Dialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder = builder.setIcon(R.drawable.ic_launcher);
+        builder = builder.setTitle("Mi primer Dialog!");
+        dialog = builder.create();
+        return dialog;
+    }
+
+    @Override
+    public void onClick(View v) {
+        onCreateDialog(1).show();
+        //Toast.makeText(getApplicationContext(),"Hola eso es un toast",Toast.LENGTH_SHORT).show() ;
+
     }
 }
