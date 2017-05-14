9b96a7a4e0cddf698439977e03e174f0d0f16358
Loic755 <loic.roubaud@gmail.com>
2017-05-14 16:25:29 -0500

    This is first test commit

--- app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
+++ app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
@@ -2,38 +2,12 @@ package exos.lujan.jose.joselujanexos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button boton;
-    CheckBox checkR,checkA,checkAm;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        boton = (Button) findViewById(R.id.button);
-        boton.setOnClickListener(this);
-
-        checkR = (CheckBox)  findViewById(R.id.uno);
-        checkA = (CheckBox)  findViewById(R.id.dos);
-        checkAm = (CheckBox)  findViewById(R.id.tres);
-
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        StringBuffer result = new StringBuffer();
-        result.append("Rojo: ").append(checkR.isChecked());
-        result.append("Azul: ").append(checkA.isChecked());
-        result.append("Amarillo: ").append(checkAm.isChecked());
-       // Toast.makeText(getApplicationContext(),"Hola eso es un toast",Toast.LENGTH_SHORT).show() ;
-        //Toast.makeText(MainActivity.this,result.toString(),Toast.LENGTH_SHORT).show();
-        Toast.makeText(getApplicationContext(),result.toString(),Toast.LENGTH_SHORT).show();
     }
 }
