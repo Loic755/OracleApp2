4a8700c6e4a1167de8bfcbf238563ac27dcb3f26
Loic755 <loic.roubaud@gmail.com>
2017-05-16 16:33:05 -0500

    App Oracle

--- app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
+++ app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
@@ -2,12 +2,27 @@ package exos.lujan.jose.joselujanexos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl("http://www.google.com");
+
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                return false;
+            }
+
+
+
+        });
     }
 }
