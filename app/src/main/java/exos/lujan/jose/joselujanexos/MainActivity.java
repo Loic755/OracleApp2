35307b65b5a83900fd74244514578161d26f7971
Loic755 <loic.roubaud@gmail.com>
2017-05-20 10:30:37 -0500

    Working REST GET

--- app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
+++ app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
@@ -9,33 +9,63 @@ import android.os.Bundle;
 import android.provider.Settings.System;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 import java.io.IOException;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+import static android.R.attr.data;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    Button boton;
+    EditText edittext;
+    EditText editTextOcudrIp;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new LongOperation().execute("");
+
+        boton = (Button) findViewById(R.id.button1);
+        boton.setOnClickListener(this);
+
+        edittext = (EditText) findViewById(R.id.editText);
+        edittext.setText("6668901223");
+
+        editTextOcudrIp  = (EditText) findViewById(R.id.editText2);
+        editTextOcudrIp.setText("10.75.92.209");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.button1:
+                //Toast.makeText(getApplicationContext(),edittext.getText().toString(),Toast.LENGTH_SHORT).show();
+                String[] myTaskParams = { edittext.getText().toString(),editTextOcudrIp.getText().toString()};
+               new LongOperation().execute(myTaskParams);
+                break;
+        }
     }
 
     private class LongOperation extends AsyncTask<String, Void, String> {
+
+        String msisdn = null;
+
         @Override
         protected String doInBackground(String... params) {
+            String msisdn = params[0];
+            String ocudrIp = params[1];
+            //Toast.makeText(getApplicationContext(),"test",Toast.LENGTH_SHORT).show();
 
-            String msisdn = "6668901223";
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
                     //.url("http://10.75.92.209:8787/rs/msr/sub/MSISDN/6668901223/data/")
-                    .url("http://10.75.92.209:8787/rs/msr/sub/MSISDN/"+msisdn+"/data/")
+                    .url("http://"+ocudrIp+":8787/rs/msr/sub/MSISDN/"+msisdn+"/data/")
                     .build();
-
-            Log.d("RESPONSE5 : ","RESPONSE5 : ");
 
             Response response = null;
             String Fresponse = "Nothing to return";
@@ -43,18 +73,17 @@ public class MainActivity extends AppCompatActivity {
             try {
                 response = client.newCall(request).execute();
                 Fresponse = response.body().string();
-                Log.d("RESPONSE4 : ",Fresponse);
+                Log.d("RESPONSE : ",Fresponse);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
             return Fresponse;
         }
 
         @Override
         protected void onPostExecute(String result) {
             TextView txt = (TextView) findViewById(R.id.output);
-            txt.setText("OCUDR responded to REST GET request: "+result);
+            txt.setText("OCUDR responded to REST request: "+result);
         }
 
         @Override
