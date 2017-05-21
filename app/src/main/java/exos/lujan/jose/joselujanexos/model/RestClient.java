60fc9701020bfafd636583d03b5221126d768765
Loic755 <loic.roubaud@gmail.com>
2017-05-21 15:46:15 -0500

    Non working reetrofit GET

---
+++ app/src/main/java/exos/lujan/jose/joselujanexos/model/RestClient.java
@@ -0,0 +1,30 @@
+package exos.lujan.jose.joselujanexos.model;
+
+//import exos.lujan.jose.joselujanexos.service.ApiInterface;
+
+/**
+ * Created by Quezacoo on 5/21/2017.
+ */
+/*
+public class RestClient {
+
+    private static ApiInterface REST_CLIENT;
+    private static String BASE_URL = "base_url";
+    static {
+        setupRestClient();
+    }
+
+    private RestClient() {}
+
+    public static ApiInterface get() {
+        return REST_CLIENT;
+    }
+
+    private static void setupRestClient() {
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(BASE_URL)
+                .build();
+        REST_CLIENT = restAdapter.create(ApiInterface.class);
+    }
+}
+*/
---
+++ app/src/main/java/exos/lujan/jose/joselujanexos/service/ApiInterface.java
@@ -0,0 +1,14 @@
+package exos.lujan.jose.joselujanexos.service;
+
+import retrofit2.http.Field;
+import retrofit2.http.POST;
+
+/**
+ * Created by Quezacoo on 5/21/2017.
+ */
+/*
+public interface ApiInterface {
+    @POST("url")
+    void getLoginResponse(@Field("username") String username , @Field("password") String password,
+                          @Field("clientId") String clientId , Callback<LoginResponse> cb);
+}*/
