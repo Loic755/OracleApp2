9ebcba1eab6aa4af55de0475fea300888607d74e
Loic755 <loic.roubaud@gmail.com>
2017-06-03 18:06:04 -0500

    Working !

--- app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
+++ app/src/main/java/exos/lujan/jose/joselujanexos/MainActivity.java
@@ -1,13 +1,39 @@
 package exos.lujan.jose.joselujanexos;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.service.chooser.ChooserTarget;
+import android.service.chooser.ChooserTargetService;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent intent;
+   // ChooserTargetService chooser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       /* intent = new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        String[] to = {"loic.roubaud@gmail.com"};
+        intent.putExtra(Intent.EXTRA_EMAIL,to);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Hi this is sent from android app");
+        intent.putExtra(Intent.EXTRA_TEXT, "TEXT");
+        intent.setType("message/rfc822");
+        Intent chooser=Intent.createChooser(intent, "Send email");
+        startActivity(chooser);*/
+
+        intent = new Intent(Intent.ACTION_DIAL);
+       intent.setData(Uri.parse("tel:30031201234"));
+
+
+        startActivity(intent);
+
+
     }
 }
---
+++ app/src/main/java/exos/lujan/jose/joselujanexos/MyFirebaseInstanceIdService.java
@@ -0,0 +1,27 @@
+package exos.lujan.jose.joselujanexos;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Quezacoo on 6/3/2017.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("My firebase id ", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(refreshedToken);
+    }
+
+}
